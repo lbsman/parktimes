d3f6e22890f946616160ad29211a2683ac249dc0
Josh -_- <jstwrenn@gmail.com>
2021-05-01 01:24:41 -0400

    Fixing telegram

--- index.js
+++ index.js
@@ -261,19 +261,13 @@ function updateWeight(dBstuff, req){
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const TelegramBot = require('node-telegram-bot-api');
+const token = '1756957219:AAH5vopV3EmHJ0bWIc8Ktblcsd12IOGvKUY';
+const bot = new TelegramBot(token, {polling: true});
 
-const TOKEN = process.env.TELEGRAM_TOKEN || '1756957219:AAH5vopV3EmHJ0bWIc8Ktblcsd12IOGvKUY';
-const TelegramBot = require('node-telegram-bot-api');
-const options = {
-  webHook: {
-    port: process.env.PORT
-  }
-};
+// const url = process.env.APP_URL || `https://jwparktimes.herokuapp.com:`;
+// const bot = new TelegramBot(TOKEN, options);
 
-const url = process.env.APP_URL || `https://https://jwparktimes.herokuapp.com:${port}`;
-const bot = new TelegramBot(TOKEN, options);
-
-bot.setWebHook(`${url}/bot${TOKEN}`);
+// bot.setWebHook(`${url}/bot${TOKEN}`);
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
